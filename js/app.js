a666c7f7a5bb2a44ece0727d7e7bed2d328448f1
Mani Raj <Mani Raj>
2016-10-11 18:13:05 +0530

    adding folder images

--- js/app.js
+++ js/app.js
@@ -1,8 +1,46 @@
 (function () {
     'use strict';
-    x = "hello";
-    angular.module('myFirstApp', [])
 
-    .controller('MyFirstController',function(){
-    });
+    var myFirstApp = angular.module('myFirstApp', [])
+
+    myFirstApp.controller('MyFirstController',MyFirstController);
+    
+    MyFirstController.$inject = ['$scope', '$filter'];
+    function MyFirstController($scope, $filter) {
+        $scope.name = "";
+        $scope.sayHello = function () {
+            return "Hello Dear ";
+        };
+        $scope.totalValue = 0;
+
+        $scope.displayTotal = function () {
+            var totalNameValue = CalculateValueofString($scope.name);
+            $scope.totalValue = totalNameValue;
+        };
+
+        function CalculateValueofString(str) {
+            var tot = 0;
+            for (var i = 0; i < str.length; i++) {
+                tot += str.charCodeAt(i);
+            }
+            return tot;
+        }
+
+        $scope.upper = function () {
+            var upCase = $filter('uppercase');
+            $scope.name = upCase($scope.name);
+            $scope.displayTotal();
+        };
+    }
+
+    //myFirstApp.controller('DIController', DIController);
+    //function DIController($scope, $filter) {
+    //    $scope.upper = function () {
+    //        var upCase = $filter('uppercase');
+    //        $scope.name = upCase($scope.name);
+    //        $scope.displayTotal();
+    //    };
+    //}
+
+
 })();
